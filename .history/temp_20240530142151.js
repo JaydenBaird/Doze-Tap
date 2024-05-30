dceb52863cb2962f0a8df39f545ef7b5b13cd4d4
JaydenBaird <bairdjayden5114@gmail.com>
2024-05-30 14:50:14 -0400

    added buttons css and also adaptive pricing

---
+++ .history/temp_20240530142151.js
@@ -0,0 +1,119 @@
+let points = 0 // point element
+let tracker = document.getElementById("tracker") //the points being tracked.
+let sheets = 0 // an upgrade to the basic clicker.
+let sheetsTracker = document.getElementById("sheets") //tracking the sheets
+let sheetC = 10
+let pillows = 0 // pillows element
+let pillowsTracker = document.getElementById("pillows") // tracking pillow element
+let beds = 0 // bed element
+let bedsTracker = document.getElementById("beds") // tracking beds
+let noisesmachine = 0 // noisemachine element
+let noisesmachineTracker = document.getElementById("noisesmachine") //tracking noisemachine
+let acs = 0 // acs element
+let acsTracker = document.getElementById("acs") // tracking acs
+let cps = 0 // cps element
+let cpsTracker = document.getElementById("cps") // tracking the cps
+
+
+
+function checkprice()  {
+    if (points >= 10) {
+        $sheets.disabled = false
+    }
+    else {
+        $sheets.disabled = true
+    }
+    if (points >= 50) {
+        $pillows.disabled = false
+    }
+    else {
+        $pillows.disabled = true
+    }
+    if (points >= 250) {
+        $beds.disabled = false
+    }
+    else {
+        $beds.disabled = true
+    }
+    if (points >= 500) {
+        $noisemachine.disabled = false
+    }
+    else {
+        $noisemachine.disabled = true
+    }
+    if (points >= 1000) {
+        $acs.disabled = false
+    }
+    else {
+        $acs.disabled = true
+    }
+}
+
+
+
+
+
+function increasePoints () {
+    points = points + 1 + (sheets * .2)
+    tracker.innerText = points.toFixed(2) +  ' Time Left'
+}
+function purchaseSheets () {
+    if (points >= 10) {
+        points = points - 10
+        tracker.innerText = points.toFixed(2) + " Seconds"
+        sheets = sheets + 1
+        sheetsTracker.innerText = sheets + ' Sheets Owned'
+        acsC.inner = ("Cost: ") + (10 + asc ** 2.225).toFixed(1)
+    } else {
+        alert("Not enough time! You need at least 10 seconds to buy a sheet")
+    }
+}
+function purchasePillows () {
+    if (points >= 50) {
+        points = points - 50
+        tracker.innerText = points.toFixed(2) + " Seconds"
+        pillows = pillows + 1
+        pillowsTracker.innerText = pillows + ' Pillows Owned'
+    } else {
+        alert("Not enough time! You need at least 50 seconds to buy a pillow")
+    }
+}
+function purchaseBeds () {
+    if (points >= 250) {
+        points = points - 250
+        tracker.innerText = points.toFixed(2) + " Seconds"
+        beds = beds + 1
+        bedsTracker.innerText = beds + " Beds Owned"
+    } else {
+        alert("Not enough time! You need at least 250 seconds to buy a Bed")
+    }
+}
+function purchaseNoisesmachine () {
+    if (points >= 500) {
+        points = points - 500
+        tracker.innerText = points.toFixed(2) + " Seconds"
+        noisesmachine = noisesmachine + 1
+        noisesmachineTracker.innerText = noisesmachine + " Noise Machines Owned"
+    } else {
+        alert("Not enough time! You need at least 500 seconds to buy Noise Machine")
+    }
+}
+function purchaseAcs () {
+    if (points >= 1000) {
+        points = points - 1000
+        tracker.innerText = points.toFixed(2) + " Seconds"
+        acs = acs + 1
+        acsTracker.innerText = acs + " Acs Owned"
+        acsC.inner = ("Cost: ") + (10 + asc ** 2.225).toFixed(1)
+    } else {
+        alert("Not enough time! You need at least 10000 seconds to buy Acs")
+    }
+}
+function gameLoop () {
+    points = points + (1 * pillows) + (5 * beds) + (10 * noisesmachine) + (25 * acs) 
+    cps = (1 * pillows) + (5 * beds) + (10 * noisesmachine) + (25 * acs) 
+    tracker.innerText = points.toFixed(2) + " Time Left"
+    cpsTracker.innerText = cps + " Seconds of sleep per second"
+}
+setInterval(gameLoop, 1000)
+setInterval(checkprice, 10)
